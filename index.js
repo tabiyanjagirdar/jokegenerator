8032ef034d5d641f33748e7609d9c9db4a7052eb
Tabiyan Jagirdar <157291665+tabiyanjagirdar@users.noreply.github.com>
2024-04-29 01:12:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const  jokeConatiner = document.querySelector('#joke');
+const btn = document.querySelector('#btn');
+const URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+
+let getJoke = async() =>{
+    jokeConatiner.classList.remove('fade')
+     let response = await fetch(URL)
+    let data = await response.json();
+    jokeConatiner.innerHTML = `${data.joke}`
+    jokeConatiner.classList.add('fade')
+
+    // .then(data => data.json())
+    // .then(data => {
+    //     
+    // });
+}
+
+
+
+btn.addEventListener('click',getJoke)
+
+getJoke();
+
